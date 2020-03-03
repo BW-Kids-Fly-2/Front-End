b3505b2648e79697a8e980b93377a9fe7b61ac36
Cristina Edens <edens.cristina@gmail.com>
2020-03-02 19:33:40 -0500

    attempting first PR

--- bw-kids-fly-2/src/App.js
+++ bw-kids-fly-2/src/App.js
@@ -1,29 +1,22 @@
 import React from 'react';
+import Forms from './React-I-components/ParentSignUp';
+import MainParentSignIn from './React-I-components/MainParentSignIn';
+import Homepage from './React-I-components/Homepage';
+import SignUpForms from './React-I-components/KFCSignUp';
+import Kfcsignin from './React-I-components/KFClogin';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
-
-import ButtonLogin from "./React-II-Components/ButtonLogin";
-import ButtonRegister from "./React-II-Components/ButtonRegister";
-import ParentProfile from "./React-II-Components/ParentProfile";
-import AssistantProfile from "./React-II-Components/AssistantProfile";
-import AddTrip from "./React-II-Components/AddTrip";
-import PrivateRoute from './React-II-Components/Authorization/PrivateRoute';
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
     <div className="App">
-      <header className="App-header">
-        BW-Kids-Fly-2
-      </header>
-      <AddTrip/>
-      <Switch>
-        <Route exact path="/authorization-register" component={ButtonRegister}/>
-        <Route exact path="/authorization-login" component={ButtonLogin}/>
-      <PrivateRoute exact path="/parent" component={ParentProfile}/>
-      <PrivateRoute exact path="/assistant" component={AssistantProfile}/>
-      <PrivateRoute exact path="/add-trip" component={AddTrip}/>
-      </Switch>
+      <Homepage />
+      <Route path="/parent-registration" component={Forms} />
+
+      <Route path="/parent-signin" component={MainParentSignIn} />
+      <Route path="/kfc-register" component={SignUpForms} />
+      <Route path="/kfc-signin" component={Kfcsignin} />
     </div>
     </Router>
   );
